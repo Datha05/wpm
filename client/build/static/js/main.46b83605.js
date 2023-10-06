5c3d17ca50743f79b67cc5eec0fcd08b26ffa25b
Datha05 <dathaprasad5305@gmail.com>
2023-10-06 20:19:32 +0530

    modifyicon

--- client/build/static/js/main.46b83605.js
+++ client/build/static/js/main.46b83605.js
@@ -34965,16 +34965,37 @@
                 return n.apply(this, arguments);
               };
             })();
-          return (0, yc.jsx)("div", {
+          return (0, yc.jsx)
+          
+          
+          ("div", {
+            
             className: "form-container ",
             children: (0, yc.jsxs)(tp, {
               layout: "vertical",
               onFinish: n,
               className: "register-form",
-              children: [
+              children: [(0, yc.jsx)("h1", {
+                className: "text-center",
+                children: "Doctor App",
+              }),
+                (0, yc.jsx)
+                ("h2", {
+                  className: "text-center",
+                  children: [
+                    yc.jsx("svg", {
+                      xmlns: "http://www.w3.org/2000/svg",
+                      viewBox: "0 0 448 512",
+                      className: "icon",
+                      children: yc.jsx("path", {
+                        d: "M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-96 55.2C54 332.9 0 401.3 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7c0-81-54-149.4-128-171.1V362c27.6 7.1 48 32.2 48 62v40c0 8.8-7.2 16-16 16H336c-8.8 0-16-7.2-16-16s7.2-16 16-16V424c0-17.7-14.3-32-32-32s-32 14.3-32 32v24c8.8 0 16 7.2 16 16s-7.2 16-16 16H256c-8.8 0-16-7.2-16-16V424c0-29.8 20.4-54.9 48-62V304.9c-6-.6-12.1-.9-18.3-.9H178.3c-6.2 0-12.3 .3-18.3 .9v65.4c23.1 6.9 40 28.3 40 53.7c0 30.9-25.1 56-56 56s-56-25.1-56-56c0-25.4 16.9-46.8 40-53.7V311.2zM144 448a24 24 0 1 0 0-48 24 24 0 1 0 0 48z",
+                      }),
+                    }),
+                  ],
+                }),
                 (0, yc.jsx)("h3", {
                   className: "text-center",
-                  children: "Login From",
+                  children: "Login",
                 }),
                 (0, yc.jsx)(tp.Item, {
                   label: "Email",
@@ -35056,9 +35077,13 @@
                 onFinish: n,
                 className: "register-form",
                 children: [
+
+                    
+
+
                   (0, yc.jsx)("h3", {
                     className: "text-center",
-                    children: "Register From",
+                    children: "Register",
                   }),
                   (0, yc.jsx)(tp.Item, {
                     label: "Name",
@@ -40302,6 +40327,20 @@
                           required: !0,
                           children: (0, yc.jsx)(sb.RangePicker, {
                             format: "HH:mm",
+                          }),
+                        }),
+                      }),
+                      (0, yc.jsx)(Gm, {
+                        xs: 24,
+                        md: 24,
+                        lg: 8,
+                        children: (0, yc.jsx)(tp.Item, {
+                          label: "Certificate",
+                          name: "Certificate",
+                          required: !0,
+                          children: (0, yc.jsx)($h, {
+                            type: "file",
+                            // placeholder: "your contact no",
                           }),
                         }),
                       }),
@@ -58851,8 +58890,9 @@
                                 {
                                   doctorId: n.doctorId,
                                   userId: e._id,
-                                  doctorInfo: o,
-                                  userInfo: e,
+                                  // doctorInfo: o,
+                                  doctorInfo: o.firstName,
+                                  userInfo: e.name,
                                   date: l,
                                   time: f,
                                 },
@@ -59009,7 +59049,7 @@
             o();
           }, []);
           var a = [
-            { title: "ID", dataIndex: "_id" },
+            { title: "Name", dataIndex: "doctorInfo" },
             {
               title: "Date & Time",
               dataIndex: "date",
@@ -59132,7 +59172,7 @@
               };
             })(),
             i = [
-              { title: "ID", dataIndex: "_id" },
+              { title: "Name", dataIndex: "userInfo" },
               {
                 title: "Date & Time",
                 dataIndex: "date",
